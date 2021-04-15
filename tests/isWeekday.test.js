034a5d4862a5f4e3cd79bcbdd8ab9302a0a599cf
bonisiweinnocent <bonisiwe.cukatha@gmail.com>
2021-04-15 10:01:03 +0200

    updated bootcamp functions tests

--- tests/isWeekday.test.js
+++ tests/isWeekday.test.js
@@ -1,21 +1,21 @@
 describe('isWeekday' , function(){
-    it('should find out if the parameter passed in is a day of the week (ie. Not a weekend day).' , function(){
+    it('should return true if the parameter passed in is Monday.' , function(){
         assert.equal(true,isWeekday("Monday"));
 
     });
-    it('should find out if the parameter passed in is a day of the week (ie. Not a weekend day).' , function(){
+    it('should return if the parameter passed in is Tuesday.' , function(){
         assert.equal(true,isWeekday("Tuesday"));
     });
-    it('should find out if the parameter passed in is a day of the week (ie. Not a weekend day).' , function(){
+    it('should return if the parameter passed in is Wednensday.' , function(){
         assert.equal(true,isWeekday("Wednesday"));
     });
-    it('should find out if the parameter passed in is a day of the week (ie. Not a weekend day).' , function(){
+    it('should return if the parameter passed in is Thursday.' , function(){
         assert.equal(true,isWeekday("Thursday"));
     });
-    it('should find out if the parameter passed in is a day of the week (ie. Not a weekend day).' , function(){
+    it('should return true if the parameter passed in is Friday.' , function(){
         assert.equal(true,isWeekday("Friday"));
     });
-    it('should find out if the parameter passed in is not a day of the week (ie.weekend day).' , function(){
+    it('should return false if the parameter passed in is Sartuday.' , function(){
         assert.equal(false,isWeekday("Sartuday")); 
 });
 });
--- tests/transportFee.test.js
+++ tests/transportFee.test.js
@@ -1,14 +1,14 @@
 describe('transportFee' , function(){
-    it('should return the right price for the morning shift' , function(){
+    it('should return the R20 for the morning shift' , function(){
         assert.equal('R20',transportFee("morning"));
 
     });
-    it('should return the right price for the afternoon shift', function(){
+    it('should return the R10 for the afternoon shift', function(){
         assert.equal('R10',transportFee("afternoon"));
 
 
 });
-it('should return the right price for the nightshift', function(){
+it('should return the -free for the nightshift', function(){
     assert.equal('free',transportFee("nightshift"));
 });
 });
